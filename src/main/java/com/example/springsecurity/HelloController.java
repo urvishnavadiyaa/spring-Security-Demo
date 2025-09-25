747544c425aa1968700da8f5398b99f516559f56
Urvish Navadiya <urvishnavadiya2004@gmail.com>
2025-09-25 12:13:36 +0530

    modify some files

--- src/main/java/com/example/springsecurity/HelloController.java
+++ src/main/java/com/example/springsecurity/HelloController.java
@@ -1,4 +1,14 @@
 package com.example.springsecurity;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request) {
+        return "welcome to spring security" + "  " + request.getSession().getId();
+    }
 }
