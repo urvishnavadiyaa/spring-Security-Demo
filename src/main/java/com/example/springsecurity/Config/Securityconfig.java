dd6a597228b819646bd3dab593db5905c3a1241b
Urvish Navadiya <urvishnavadiya2004@gmail.com>
2025-09-25 13:07:36 +0530

    modify some files

--- src/main/java/com/example/springsecurity/Config/Securityconfig.java
+++ src/main/java/com/example/springsecurity/Config/Securityconfig.java
@@ -1,10 +1,27 @@
 package com.example.springsecurity.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Securityconfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+//                http.formLogin(Customizer.withDefaults());
+
+    }
 }
