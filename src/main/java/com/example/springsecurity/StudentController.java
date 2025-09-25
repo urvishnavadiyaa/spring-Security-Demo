c63b4b0cfeee7c5957f523c4e1bd5efe5e77cc3a
Urvish Navadiya <urvishnavadiya2004@gmail.com>
2025-09-25 13:03:25 +0530

    modify some files

---
+++ src/main/java/com/example/springsecurity/StudentController.java
@@ -0,0 +1,36 @@
+package com.example.springsecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "urvish", 43),
+            new Student(2, "roy", 75)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudent() {
+        return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getcsrftoken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        students.add(student);
+        return student;
+    }
+}
