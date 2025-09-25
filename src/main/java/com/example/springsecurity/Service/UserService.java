acb4dc5d205316a2eab9911f942180879dcb16f8
Urvish Navadiya <urvishnavadiya2004@gmail.com>
2025-09-25 15:49:01 +0530

    modify some files

---
+++ src/main/java/com/example/springsecurity/Service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.springsecurity.Service;
+
+import com.example.springsecurity.Model.Users;
+import com.example.springsecurity.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+     public Users register(Users user) {
+         user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+     }
+}
